5421cbab160b5cb3a230d476468ebe84c63e73f0
prathmesh <belhekarp.09@gmail.com>
2025-04-15 03:44:58 -0700

    ec2 creation

---
+++ variable.tf
@@ -0,0 +1,15 @@
+variable "aws_instance_type"{
+                default="t2.micro"
+}
+
+
+variable "root_storage_size"{
+        default=15
+}
+
+
+variable "default_ami"{
+        default = "ami-084568db4383264d4"
+
+
+}
